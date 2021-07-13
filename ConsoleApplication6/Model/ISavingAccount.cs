27c9667619c98797d15dc15b3df6bbc97478c294
cqm2112 <61892716+cqm2112@users.noreply.github.com>
2021-07-12 23:46:50 -0400

    first-commit

---
+++ ConsoleApplication6/Model/ISavingAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6.Model
+{
+    interface ISavingAccount
+    {
+        decimal Opening(decimal openingAmount);
+        decimal Deposit(decimal depositMoney);
+        decimal Retreats(decimal retreatsMoney);
+        decimal CheckBalance();
+    }
+}
---
+++ ConsoleApplication6/Model/SavingAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6.Model
+{
+    public class SavingAccount : ISavingAccount
+    {
+        private int IdAccount { get; set; }
+        public decimal Balance { get; set; }
+
+        public decimal Opening(decimal OpeningAmount)
+        {
+            Balance = OpeningAmount;
+            Random random = new Random();
+            var idAccount = random.Next(10000000, 999999999).ToString("D6");
+            return Convert.ToInt64(idAccount);
+        }
+
+        public decimal Deposit(decimal depositMoney)
+        {
+            Balance += depositMoney;
+            return Balance;
+        }
+
+        public virtual decimal Retreats(decimal retreatsMoney)
+        {
+            Balance -= retreatsMoney;
+            return Balance;
+        }
+
+        public decimal CheckBalance()
+        {
+            return Balance;
+        }
+    }
+}
